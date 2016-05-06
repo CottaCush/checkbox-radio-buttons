543f1fed96e5fd988dac4706671a73e4871e332e
Olajide Oye <olajide.oye@gmail.com>
2016-05-06 11:08:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+Keyboard Submit
+======================
+
+Disables or enables the browser's default behaviour of submitting a form when the enter key is pressed on any of it's children input elements.
+
+Please note that this plugin requires [jQuery](http://jquery.com).
+
+ ## Usage
+ The plugin can be used in the following ways:
+ 1. through the presence of a form's `data-` attribute.
+ 2. through the jQuery interface `$.keyboardSubmit()`
+
+
+#### Using Data attributes
+You can disable form submit by simply adding the data attribute in the following way:
+ ```html
+ <form data-keyboard-submit>
+    ...
+</form>
+ ```
+ You can also pass any valid action into the data attribute as shown below
+ ```html
+<form data-keyboard-submit="disable">
+    ...
+</form>
+ ```
+
+#### Using the jQuery interface
+You can also activate the plugin it's defined jQuery method
+
+ ```js
+ $(form).keyboardSubmit()
+ ```
+ Like using data attributes, you can also pass a valid action into the method.
+
+  ```js
+  $(form).keyboardSubmit(true)
+  ```
+
+  ## Valid Actions
+The following are valid actions that can be used with the plugin
+
+
+| Actions                               | Type              | Description                                                                                                                             |
+|---------------------------------------|-------------------|-----------------------------------------------------------------------------------------------------------------------------------------|
+| `true`, `'true'`, `'enable'`          | Boolean or String | Forms can be submitted using the enter/return key. This is the browser's default.                                                       |
+| `false`, `'false'`, `'disable'`, `''` | Boolean or String | Disable submitting forms using the keyboard. The empty string ('') here handles scenarios of forms with data attributes without a value |
---
+++ dist/keyboard-submit.js
@@ -0,0 +1,72 @@
+/**
+ * KEYBOARD SUBMIT
+ *
+ * Disable or enable the browser's default behaviour of submitting a form when the enter key is pressed on any of it's children input elements.
+ *
+ * The plugin can be enabled/disabled in two forms:
+ *  a. through the presence of a form's data attribute, with an optional value
+ *  b. through the jQuery plugin interface $(form).keyboardSubmit(action);
+ *
+ */
+
+(function($){
+    var defaults = {
+        action: 'disable',
+        formAttribute: 'data-keyboard-submit',
+        eventNamespace: '.Terra.form.keyboard-submit',
+        inputSelector: 'input:not([type="checkbox"]):not([type="radio"]):not([type="submit"]):not([type="button"]):not([type="range"])'
+    };
+
+    var keyboardSubmit = function(form, action) {
+        var eventName = 'keypress' + defaults.eventNamespace;
+
+        // Ensure that the form is always a jquery object
+        if (typeof form.jquery === 'undefined')
+            form = $(form);
+
+        action = (typeof action !== 'undefined') ? action : defaults.action;
+
+        var input = form.find(defaults.inputSelector);
+
+        switch (action) {
+            case false:
+            case 'false':
+            case 'disable':
+            case '': // mostly found with empty form data- attribute values
+                disable();
+                break;
+
+            case true:
+            case 'true':
+            case 'enable':
+                enable();
+                break;
+        }
+
+        function disable() {
+            $(input).off(eventName).on(eventName, function(e) {
+                if(e.which === 13) { // enter or return key
+                    e.preventDefault();
+                    return false;
+                }
+            });
+        }
+        function enable() {
+            $(input).off(eventName);
+        }
+    };
+
+    // Register as a jQuery function
+    $.fn.keyboardSubmit = function(action) {
+        return this.each(function(){
+            keyboardSubmit(this, action);
+        });
+    };
+
+    // Automatically activate using the form's data- attribute.
+    var attr = defaults.formAttribute;
+    $('[' + attr + ']').each(function () {
+        keyboardSubmit(this, $(this).attr(attr));
+    });
+
+})(jQuery);
